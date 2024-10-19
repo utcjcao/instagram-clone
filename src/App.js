f433ea5d85727a55fae054edf5f6793c4bd7fd92
utcjcao <chrisj.cao@mail.utoronto.ca>
2024-10-19 17:48:33 -0400

    redoing it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { AuthProvider, AuthContext } from "./components/AuthProvider";
-import { Routes } from "./routes/index.js";
+import { Router, Routes, Route } from "react-router-dom";
+import AnonymousLayout from "./layouts/AnonymousLayout"
+import MainLayout from "./layouts/MainLayout"
+import FeedPage from "./pages/FeedPage"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import ProfilePage from "./pages/ProfilePage"
+import SignUpPage from "./pages/SignUpPage"
+
 
 const App = () => {
-  return <Routes isAuthorized={true} />;
+  return (<AuthProvider>
+    <Router>
+      <Routes>
+    <Route element ={AnonymousLayout}>
+      <Route path="/" element ={}></Route>
+    </Route>
+    <Route element ={MainLayout}>
+      
+    </Route>
+      </Routes>
+    </Router>
+  </AuthProvider>);
 };
 
 export default App;
--- src/layouts/AnonymousLayout.js
+++ src/layouts/AnonymousLayout.js
@@ -4,39 +4,7 @@ import { AuthProvider, AuthContext } from "../components/AuthProvider";
 
 const Layout = () => {
   const { isLoggedIn } = useContext(AuthContext);
-  return (
-    <div>
-      <header>
-        <h1>My Instagram Clone</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/images">Explore</a>
-            </li>
-            <li>
-              <a href="/profile">Profile</a>
-            </li>
-            <li>
-              <a href="/login">
-                {isLoggedIn ? <span>logout</span> : <span>login</span>}
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <Outlet />
-      </main>
-
-      <footer>
-        <p>footer for now</p>
-      </footer>
-    </div>
-  );
+  return <div>ur not logged in dingus</div>;
 };
 
 export default Layout;
